55ca3ab098d922f2a114ba982ef187b0a89a589d
danielalexlarsen <danielalexlarsen@gmail.com>
2023-10-01 14:29:05 +0200

    no mobile view working, but lgtm

--- my-website/src/App.vue
+++ my-website/src/App.vue
@@ -8,22 +8,26 @@
       <pre data-prefix="$"><code1>Who am I?</code1></pre>
       <pre data-prefix=">" class="text-success"><code>   My name is Daniel Larsen.</code></pre> 
       <pre data-prefix="$"><code1>What do I do?</code1></pre> 
-      <pre data-prefix=">" class="text-success"><code>   Currently finishing a degree in Software Enginnering at <a href="https://www.aau.dk/uddannelser/bachelor/software">AAU (Aalborg University).</a></code></pre>
+      <pre data-prefix=">" class="text-success"><code>   Currently finishing a degree in Software Enginnering at <a style="text-decoration: underline;" href="https://www.aau.dk/uddannelser/bachelor/software">AAU (Aalborg University).</a></code></pre>
       <pre data-prefix="$"><code1>And besides that?</code1></pre>
-      <pre data-prefix=">" class="text-success"><code>   Also employed at Aalborg University under the <a href="https://vbn.aau.dk/da/organisations/cyber-security-group">Cyber Security Group, Institute of Electronic Systems.</a></code></pre>
+      <pre data-prefix=">" class="text-success"><code>   Also employed at Aalborg University under the <a style="text-decoration: underline;" href="https://vbn.aau.dk/da/organisations/cyber-security-group">Cyber Security Group, Institute of Electronic Systems.</a></code></pre>
       <pre data-prefix="$"><code1>Can you elaborate?</code1></pre>
-      <pre data-prefix=">" class="text-success"><code>   I primarily teach cybersecurity through our <a href="https://github.com/aau-network-security/haaukins">CTF platform, Haaukins.</a></code></pre>
+      <pre data-prefix=">" class="text-success"><code>   I primarily teach cybersecurity through our <a style="text-decoration: underline;"  href="https://github.com/aau-network-security/haaukins">CTF platform, Haaukins.</a></code></pre>
   </div>
 <footer>
   <nav>
-    <header style="margin-bottom: 20px;" >Check me out here</header>
+    <header style="margin-bottom: 20px;" >Get in touch with me</header>
     <a href="https://github.com/danielalexlarsen">
       <img src="./assets/github-mark-white.svg" class="h-10 logo" alt="GitHub logo"/>
     </a>
     <a href="https://www.linkedin.com/in/daniel-alexander-larsen/"> 
       <img src="./assets/LinkedIn-Logos/LI-Logo.png" class="h-10 logo" alt="LinkedIn logo"/>
     </a>
-    {{ new Date().getUTCFullYear() }}
+    <a> Discord: 0xd 
+    </a>
+  <div style="margin-top: 20px;">
+      {{ new Date().getUTCFullYear() }}
+  </div>
   </nav>
 </footer>
     
